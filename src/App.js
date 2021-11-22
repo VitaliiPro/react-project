f42fa62aad5920fceb200729509cf6a4fd600614
Vitalii Tarasiuk <91137464+VitaliiPro@users.noreply.github.com>
2021-11-22 19:41:47 +0200

    react-intro

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Post from './Post'
+import './App.css';
+
+function App() {
+  const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+  const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+  return (
+    <Post author={{
+      name: "Anakin skywalker",
+      photo: ANAKIN_IMAGE,
+      nickname: "@dart_vader"
+   }}
+   content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+   image={RAY_IMAGE}
+   date={"26 февр."}
+    />
+  );
+}
+
+export default App;
