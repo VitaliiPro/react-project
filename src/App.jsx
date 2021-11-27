55e0b54b1043259f393bb5750336ff59285f74ff
Vitalii Tarasiuk <91137464+VitaliiPro@users.noreply.github.com>
2021-11-27 14:43:46 +0200

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Post from './Post'
+import Post from './Components/Post';
 import './App.css';
+import './Components/Post.css';
 
 function App() {
   const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
